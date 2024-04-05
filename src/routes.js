5b22e34a5a8adb027bdcee9385e664372a9b5373
Everton Deplá <evertondutra257@gmail.com>
2024-04-05 19:08:05 -0300

    Configuração do Model de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.status(200).json({ message: 'Hello World' }),
-);
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Everton2',
+    email: 'everton2@email.com',
+    password_hash: 'dejdsjadkd',
+  });
+
+  return response.status(201).json(user);
+});
 
 export default routes;
